6545b6c644379d06ec55f13ff2ee4ee9358db0f6
Sneh Mehta <smehta@colum.edu>
2021-02-13 23:02:49 -0600

    Take 2 Master Folder

---
+++ ApplicationDesign/js-primer/js/basics/01-output.js
@@ -0,0 +1,6 @@
+// This statement prints out a message in a model window:
+// alert("Hello World! 🌎");
+
+console.log("Hello Console! 👋");
+
+// document.body.insertAdjacentHTML("beforeend", "<p>Hello from JS</p>")
---
+++ ApplicationDesign/quiz-game/js/quiz-game/trivia-items.js
@@ -0,0 +1,90 @@
+const triviaItems = [
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Who was the Roman god of fire?",
+    correct_answer: "Vulcan",
+    incorrect_answers: ["Apollo", "Jupiter", "Mercury"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "Which of the following Mesopotamian mythological figures was NOT a deity?",
+    correct_answer: "Enkidu",
+    incorrect_answers: ["Enki", "Enlil", "Enkimdu"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "hard",
+    question: "In Greek Mythology, who was the daughter of King Minos?",
+    correct_answer: "Ariadne",
+    incorrect_answers: ["Athena", "Ariel", "Alana"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which figure from Greek mythology traveled to the underworld to return his wife Eurydice to the land of the living?",
+    correct_answer: "Orpheus",
+    incorrect_answers: ["Hercules", "Perseus", "Daedalus"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "medium",
+    question: "A minotaur is half human half what?",
+    correct_answer: "Bull",
+    incorrect_answers: ["Cow", "Horse", "Eagle"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which of these mythological creatures is said to be half-man and half-horse?",
+    correct_answer: "Centaur",
+    incorrect_answers: ["Minotaur", "Pegasus", "Gorgon"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "In African mythology, Anansi is a trickster and storyteller who takes the shape of which animal?",
+    correct_answer: "Spider",
+    incorrect_answers: ["Wild dog", "Monkey", "Crocodile"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "The Nike apparel and footwear brand takes it&#039;s name from the Greek goddess of what?",
+    correct_answer: "Victory",
+    incorrect_answers: ["Courage", "Strength", "Honor"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "According to Japanese folklore, what is the favorite food of the Kappa.",
+    correct_answer: "Cucumbers",
+    incorrect_answers: ["Kabocha", "Nasu", "Soba"],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the name of the first human being in Norse mythology?",
+    correct_answer: "Ask",
+    incorrect_answers: ["Asmund", "Ake", "Asger"],
+  },
+];
+
+export default triviaItems;
