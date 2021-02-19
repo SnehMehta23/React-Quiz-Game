6dd14a79afd4fb497005478332b2c8bd7a41470d
Sneh Mehta <smehta@colum.edu>
2021-02-19 02:08:52 -0600

    Almost done just have to implement the reset button and the conditional

--- ApplicationDesign/react-quiz-game/src/demos/state/custom-increment.js
+++ ApplicationDesign/react-quiz-game/src/demos/state/custom-increment.js
@@ -1,5 +1,49 @@
+import { useState } from "react";
+
 function CustomIncrement() {
-  return <p>Hello</p>;
+  const [amount, setAmount] = useState(0);
+  const [incrementCount, setIncrementCount] = useState(1);
+  const onAddButtonClick = () => {
+    setAmount(amount + incrementCount);
+    console.log(amount);
+  };
+
+  // let onResetButtonClick = () => {
+
+  // }
+
+  // onClick={onResetButtonClick} goes inside the reset button
+
+  const onIncrementChange = (event) => {
+    const newIncrement = parseInt(event.target.value);
+    setIncrementCount(newIncrement);
+  };
+
+  const buttonStyle = {
+    marginLeft: "0.5rem",
+  };
+
+  return (
+    <div>
+      <p>The current value is: {amount}.</p>
+      <p>The current increment is: {incrementCount}.</p>
+      <button value={incrementCount} onClick={onAddButtonClick}>
+        Add {incrementCount}
+      </button>
+      <button style={buttonStyle}>Reset</button>
+      <br />
+      <label htmlFor="increment">Increment: </label>
+      <input
+        id="increment"
+        type="range"
+        min="-100"
+        max="100"
+        step="1"
+        value={incrementCount}
+        onChange={onIncrementChange}
+      />
+    </div>
+  );
 }
 
 export default CustomIncrement;
