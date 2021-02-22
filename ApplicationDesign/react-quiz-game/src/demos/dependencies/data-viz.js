d95078d9588edc14a168798104fb374128fc4b34
Sneh Mehta <smehta@colum.edu>
2021-02-22 12:27:02 -0600

    Optional challenge with Recharts done

---
+++ ApplicationDesign/react-quiz-game/src/demos/dependencies/data-viz.js
@@ -0,0 +1,30 @@
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+
+function DataViz() {
+  const data = [
+    { name: "Monday", temp: 23 },
+    { name: "Tuesday", temp: 20 },
+    { name: "Wednesday", temp: 18 },
+    { name: "Thursday", temp: 24 },
+    { name: "Friday", temp: 32 },
+  ];
+
+  return (
+    <LineChart width={600} height={300} data={data}>
+      <Line type="monotone" dataKey="temp" stroke="#8884d8" strokeWidth="4px" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="name" />
+      <YAxis dataKey="temp" />
+      <Tooltip />
+    </LineChart>
+  );
+}
+
+export default DataViz;
--- ApplicationDesign/react-quiz-game/src/home/home-page.js
+++ ApplicationDesign/react-quiz-game/src/home/home-page.js
@@ -1,8 +1,12 @@
+import { LineChart } from "recharts";
+import DataViz from "../demos/dependencies/data-viz.js";
+
 function HomePage() {
   return (
     <main>
       <h1>Home Page 🏡</h1>
       <p>This is the home page</p>
+      <DataViz />
     </main>
   );
 }
